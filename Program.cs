72af1cc4fac205ee4ddffafe983644eed7dcb642
Michael Kragh <mikr71394@edu.ucl.dk>
2025-07-26 21:02:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Channels;
+
+namespace CarAppMike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+       
+         
+
+         Console.WriteLine("Indtast bilmærke: ");
+         string brand = Console.ReadLine();
+
+         Console.WriteLine("Indtast bilmodel: ");
+         string model = Console.ReadLine();
+
+         Console.WriteLine("Indtast årgang: ");
+         int year = int.Parse(Console.ReadLine());
+
+         Console.WriteLine("Indtast geartype (A for automatisk, M for manuel): ");
+         char gearType = (Console.ReadLine()[0]);
+
+         Console.WriteLine($"Dette er biloplysningerne {brand} {model} {year} {gearType}");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
